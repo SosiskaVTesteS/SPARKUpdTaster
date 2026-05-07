43145f6d2529fec2a986137ccbe0925ec4ba5090
SosiskaVTesteS <sosiskavtestes228@gmail.com>
2026-05-08 02:10:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ function openShare() {
     
     const targetUrl = "https://github.com/SPARK";
     
-    // Цвет D4AF37 — это классическое "Золото" (Metallic Gold)
-    // Оно выглядит гораздо спокойнее и натуральнее
-    const goldColor = "d4af37"; 
+    // Мягкий золотой цвет (без неона)
+    const goldColor = "c5a021"; 
     const bgColor = "0d1117";
     
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${targetUrl}&color=${goldColor}&bgcolor=${bgColor}`;
     
     modal.style.display = 'flex';
     setTimeout(() => modal.classList.add('active'), 10);
+}
+
+function closeShare() {
+    const modal = document.getElementById('shareModal');
+    modal.classList.remove('active');
+    setTimeout(() => modal.style.display = 'none', 400);
 }
