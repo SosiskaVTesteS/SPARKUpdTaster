bc3ca2da4b17ba5670632dc48eb0e0daa41bb0e2
SosiskaVTesteS <sosiskavtestes228@gmail.com>
2026-05-08 16:18:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,12 @@ function focusSystem(element) {
         anchor.classList.remove('focused');
         dimmer.classList.remove('active');
     } else {
-        // Чистим фокус у всех
         document.querySelectorAll('.sys-anchor').forEach(el => el.classList.remove('focused'));
-        // Ставим фокус на текущую
         anchor.classList.add('focused');
         dimmer.classList.add('active');
     }
 }
 
-// Закрытие по фону
 document.getElementById('spaceDimmer').onclick = function() {
     this.classList.remove('active');
     document.querySelectorAll('.sys-anchor').forEach(el => el.classList.remove('focused'));
